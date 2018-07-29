0a2a46bc72a7d0a845e43966199c4cf33781f73f
xlauko <xlauko@mail.muni.cz>
2018-07-29 14:42:22 +0200

    Initialize idt.

--- include/kernel/dt.hpp
+++ include/kernel/dt.hpp
@@ -3,18 +3,21 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#define PACKED __attribute__((packed))
+
 namespace kernel {
 
-    struct gdt_entry {
-        uint16_t limit_low;
-        uint16_t base_low;
-        uint8_t base_middle;
-        uint8_t access;
-        uint8_t granularity;
-        uint8_t base_high;
-    } __attribute__((packed));
-
     struct gdt {
+
+        struct item {
+            uint16_t limit_low;
+            uint16_t base_low;
+            uint8_t base_middle;
+            uint8_t access;
+            uint8_t granularity;
+            uint8_t base_high;
+        } PACKED;
+
         uint16_t limit;
         uint32_t base;
 
@@ -24,8 +27,29 @@ namespace kernel {
         void set( uint32_t base, uint32_t limit, uint8_t access, uint8_t gran );
 
         static void init();
-    } __attribute__((packed));
+    } PACKED;
 
+
+    struct idt {
+
+        struct item {
+            uint16_t base_low;
+            uint16_t selector;
+            uint8_t zero;
+            uint8_t flags;
+            uint16_t base_high;
+        } PACKED;
+
+        uint16_t limit;
+        uint32_t base;
+
+        static constexpr size_t size = 256;
+
+        template< size_t idx >
+        void set( uint32_t isr, uint16_t selector, uint8_t flags );
+
+        static void init();
+    } PACKED;
 
     namespace dt {
         void init();
--- src/kernel/dt.cpp
+++ src/kernel/dt.cpp
@@ -1,17 +1,21 @@
 #include <kernel/dt.hpp>
 
+#include <string.h>
+
 using namespace kernel;
+
+/* GDT function */
 
 extern "C" void __gdt_flush( void * gdt );
 
 extern "C" {
-    gdt_entry table[ gdt::size ];
+    gdt::item gdtable[ gdt::size ];
     gdt gdt_ptr;
 }
 
 template< size_t idx >
 void gdt::set( uint32_t base, uint32_t limit, uint8_t access, uint8_t gran ) {
-    auto &item = table[ idx ];
+    auto &item = gdtable[ idx ];
 
     item.base_low =     (base & 0xFFFF);
     item.base_middle =  (base >> 16) & 0xFF;
@@ -23,8 +27,8 @@ void gdt::set( uint32_t base, uint32_t limit, uint8_t access, uint8_t gran ) {
 }
 
 void gdt::init() {
-    gdt_ptr.limit = ( sizeof(gdt_entry) * gdt::size ) - 1;
-    gdt_ptr.base = reinterpret_cast< uint32_t >( &table );
+    gdt_ptr.limit = ( sizeof( gdt::item ) * gdt::size ) - 1;
+    gdt_ptr.base = reinterpret_cast< uint32_t >( &gdtable );
 
     gdt_ptr.set< 0 >( 0, 0, 0, 0 );                 // null segment
     gdt_ptr.set< 1 >( 0, 0xFFFFFFFF, 0x9A, 0xCF );  // code segment
@@ -34,10 +38,41 @@ void gdt::init() {
      __gdt_flush( &gdt_ptr );
 }
 
+/* IDT functions */
+
+extern "C" {
+    idt::item idtable[ idt::size ];
+    idt idt_ptr;
+}
+
+extern "C" void __idt_flush();
+
+template< size_t idx >
+void idt::set( uint32_t base, uint16_t selector, uint8_t flags ) {
+    auto &item = idtable[ idx ];
+    item.base_low  = (base & 0xFFFF);
+    item.base_high = (base >> 16) & 0xFFFF;
+    item.selector  = selector;
+    item.zero      = 0;
+    // We must uncomment the OR below when we get to using user-mode.
+    // It sets the interrupt gate's privilege level to 3.
+    item.flags     = flags /* | 0x60 */;
+}
+
+void idt::init() {
+    idt_ptr.limit = ( sizeof( idt::item ) * idt::size ) - 1;
+    idt_ptr.base = reinterpret_cast< uint32_t >( idtable );
+
+    memset( &idtable, 0, sizeof( idt::item ) * idt::size );
+
+    __idt_flush();
+}
+
 namespace kernel::dt {
 
     void init() {
         gdt::init();
+        idt::init();
     }
 
 } // namespace dt;
